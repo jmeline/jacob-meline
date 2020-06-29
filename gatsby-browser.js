562c29077e4d097ddee247984547a4e3ea2bced0
Jacob Meline <jacob.meline@extendhealth.com>
2020-06-28 22:02:25 -0600

    Add some dracula styling

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,7 @@
 // custom typefaces
 import "typeface-montserrat"
 import "typeface-merriweather"
+import "./src/styles/global.css"
 
 // import "prismjs/themes/prism.css"
 import "gatsby-prismjs-dracula"
